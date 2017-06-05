ce4144e41d2a6398ca5d958927c3be8d5a833a78
Joan Miquel Espada Sabat <joanmi@espada.cat>
2017-06-05 20:10:55 +0200

    t1

--- answers/Palindrome.java
+++ answers/Palindrome.java
@@ -1,12 +1,38 @@
 
+import java.io.*;
+import java.util.Scanner;
 
 
 public class Palindrome
 {
 
+   //Computational cost: O(n)
+   //Space cost: O(n) due to it's a recursive version 
+   protected static boolean isPalindrome(String s)
+   {
+       if(s.length()==0 ||
+               s.length()==1)
+           return true;
+       else
+       {
+           if(s.charAt(0) == s.charAt( s.length()-1  ))
+               return isPalindrome( s.substring(1, s.length()-1) );
+       }
+       return false;
+   }
+
+
+
     public static void main(String args[])
     {
-
+	Scanner stdin = new Scanner(System.in);
+	while(stdin.hasNextLine())
+    	{
+        	String line = stdin.nextLine();
+        	System.out.print(line +": ");
+		boolean t= isPalindrome(line);
+        	System.out.println(t);
+    	}
 
 
     }
